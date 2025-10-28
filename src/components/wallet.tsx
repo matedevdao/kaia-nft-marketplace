1655975f875a5d1ff23c8db29cc6373079e19252
yj.gaia <gaiaprotocolnft@gmail.com>
2025-10-28 14:01:43 +0900

    Add wallet component integration with RainbowKit; update wallet configuration to include multiple wallet options and improve connection handling.

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { ConnectButton, getDefaultWallets, lightTheme, RainbowKitProvider, useConnectModal } from '@rainbow-me/rainbowkit';
+import { ConnectButton, getDefaultConfig, lightTheme, RainbowKitProvider, useConnectModal } from '@rainbow-me/rainbowkit';
+import {
+  kaiaWallet,
+  kaikasWallet,
+  metaMaskWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { getPublicClient } from '@wagmi/core';
 import { el } from '@webtaku/el';
 import {
-  createConfig,
   http,
   WagmiProvider
 } from 'wagmi';
@@ -18,18 +23,17 @@ declare const WALLET_CONNECT_PROJECT_ID: string;
 
 const queryClient = new QueryClient();
 
-// wagmi + rainbowkit 최신 설정
-const { connectors } = getDefaultWallets({
+const config: any = getDefaultConfig({
   appName: APP_NAME,
   projectId: WALLET_CONNECT_PROJECT_ID,
-});
-
-const config: any = createConfig({
   chains: [kaia as any],
   transports: {
     [kaia.id]: http(), // RPC를 설정
   },
-  connectors,
+  wallets: [{
+    groupName: "Recommended",
+    wallets: [kaiaWallet, kaikasWallet, metaMaskWallet, walletConnectWallet],
+  }],
   ssr: false, // (선택) SSR 사용하지 않을 경우
 });
 
